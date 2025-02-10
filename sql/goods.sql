94bc6e70eae6f8b8cb2432e4d1095f4ca770b468
SaebaRyoo <yuanddmail@163.com>
2025-02-10 20:10:18 +0800

    feat: 1.修改docker配置 2.添加数据库back up sql

--- sql/goods.sql
+++
@@ -1,9 +0,0 @@
-
--- DROP TABLE IF EXISTS public.ibuy_template;
--- CREATE TABLE IF NOT EXISTS public.ibuy_template
--- (
---     id integer NOT NULL DEFAULT nextval('ibuy_template_id_seq'::regclass),
---     name character varying,
---     spec_num character 
---     PRIMARY KEY (id),
--- )
